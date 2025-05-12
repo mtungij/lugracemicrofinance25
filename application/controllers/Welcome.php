a081b11631966ccdf65a747782e3d2b263bb673a
mtungij <jamesmtungi2013@gmail.com>
2025-05-12 15:00:47 +0300

    sms

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -764,7 +764,7 @@ $sqldata="UPDATE `tbl_customer` SET `customer_status`= 'close' WHERE `customer_i
 //sending receivable
 public function Send_reminder_automatic(){
 $today = date('Y-m-d');
- $comp_id = 100;
+ $comp_id = 164;
 $data = $this->db->query("SELECT * FROM tbl_loans l LEFT JOIN tbl_customer c ON c.customer_id = l.customer_id WHERE l.comp_id = '$comp_id' AND l.dep_status = 'open' AND l.date_show = '$today' AND l.loan_status = 'withdrawal'");
 	$auto_reminder =  $data->result();
 
